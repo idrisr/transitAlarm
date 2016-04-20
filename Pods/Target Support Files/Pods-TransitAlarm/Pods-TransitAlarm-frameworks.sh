b49a133538389ea23966c309b2336e258cd93f46
Matthew Bracamonte <Braca1637@gmail.com>
2016-04-20 10:36:47 -0500

    Create login
    add storyboard change and add file

--- Pods/Target Support Files/Pods-TransitAlarm/Pods-TransitAlarm-frameworks.sh
+++ Pods/Target Support Files/Pods-TransitAlarm/Pods-TransitAlarm-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-TransitAlarm/Alamofire.framework"
+  install_framework "Pods-TransitAlarm/Firebase.framework"
   install_framework "Pods-TransitAlarm/SWXMLHash.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-TransitAlarm/Alamofire.framework"
+  install_framework "Pods-TransitAlarm/Firebase.framework"
   install_framework "Pods-TransitAlarm/SWXMLHash.framework"
 fi
