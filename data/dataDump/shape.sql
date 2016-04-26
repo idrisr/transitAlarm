e6a44d04a3a2cf3e30c4dcbf62611d45280281dd
Idris Raja <idris.raja@gmail.com>
2016-04-26 14:46:12 -0500

    fix bug in join query

--- data/dataDump/shape.sql
+++ data/dataDump/shape.sql
@@ -9,5 +9,5 @@ CREATE TABLE shapes SELECT * FROM (
 
     SELECT s.shape_id, shape_pt_lat, shape_pt_lon, shape_pt_sequence 
     FROM metra.shapes s
-    JOIN metra.trips t ON s.shape_id = t.trip_id
+    JOIN metra.trips t ON s.shape_id = t.shape_id
 ) X
