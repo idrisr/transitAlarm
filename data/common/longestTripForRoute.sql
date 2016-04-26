33846dd65c74d55e35e0f2b31643125ac06dfedc
Idris Raja <idris.raja@gmail.com>
2016-04-25 22:17:28 -0500

    fix bug where we were selecting trip with max stop sequence value, but not necesarrily the trip with the most total stops. Still not perfect as its possible no trips has all the stops

--- data/common/longestTripForRoute.sql
+++ data/common/longestTripForRoute.sql
@@ -1,23 +1,30 @@
--- finds the trip for a route with the max number of stops.
--- if trip with max stops tied, chooses the max trip_id
-
 use DB_REPLACE_ME;
+
+-- get max counts per trip
 CREATE TABLE routeTrip AS (
-    SELECT r.agency_id, z.route_id, r.route_long_name, MAX(z.trip_id) as aTripID
-    FROM (
-        SELECT t.trip_id, t.route_id, st.stop_sequence
-        FROM trips t
-        JOIN stop_times st
-        ON t.trip_id = st.trip_id
-    ) z
-    JOIN
-        (SELECT   t.route_id, max(stop_sequence) as max_stop_sequence
+    SELECT z.route_id, max(z.trip_id) as trip_id
+    FROM
+    (   SELECT max(count_stop_sequence) as max_stop, route_id
+        FROM 
+            -- count stops per trip per route
+            ( SELECT   count(stop_sequence) as count_stop_sequence, t.route_id, t.trip_id
+                FROM     trips t
+                JOIN     stop_times st ON st.trip_id = t.trip_id
+                JOIN     stops s       ON st.stop_id = s.stop_id
+                GROUP BY t.route_id, t.trip_id
+                ASC ORDER BY  t.route_id, count_stop_sequence, t.trip_id
+            ) X
+        GROUP BY route_id
+    ) Y
+    JOIN 
+    (   SELECT   count(stop_sequence) as count_stop_sequence, t.route_id, t.trip_id
         FROM     trips t
         JOIN     stop_times st ON st.trip_id = t.trip_id
         JOIN     stops s       ON st.stop_id = s.stop_id
-        GROUP BY t.route_id
-    ) y
-    ON y.route_id = z.route_id AND y.max_stop_sequence = z.stop_sequence
-    JOIN routes r ON r.route_id = z.route_id
-    GROUP BY z.route_id, r.route_long_name, z.stop_sequence
-)
+        GROUP BY t.route_id, t.trip_id
+        ASC ORDER BY  t.route_id, count_stop_sequence, t.trip_id
+    ) Z
+    WHERE Y.max_stop = Z.count_stop_sequence 
+    AND  Z.route_id = Y.route_id
+    GROUP BY z.route_id
+);
