514f1441b6231bef62c24c075bc4aee3ea35d0ba
Idris Raja <idris.raja@gmail.com>
2016-05-02 19:33:55 -0500

    custom viewcells for agency, route, & stops

---
+++ AgencyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AgencyTableViewCell.swift
+//  TransitAlarm
+//
+//  Created by id on 5/2/16.
+//  Copyright © 2016 id. All rights reserved.
+//
+
+import UIKit
+
+class AgencyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
