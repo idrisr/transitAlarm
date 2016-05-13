bdd46c47a3bccbae02baf11d8084808a5b13fe5b
Idris Raja <idris.raja@gmail.com>
2016-05-12 23:44:44 -0500

    jettison unneeded comments

--- Support/AppDelegate.swift
+++ Support/AppDelegate.swift
@@ -11,7 +11,6 @@ import CoreData
 import CoreLocation
 import UIKit
 
-//FIXME: bug - didnt work when app was in background and hit the stop
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate {
 
@@ -24,7 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate {
 
     // FIXME: turn off compass calibration
     func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
-        // stop monitoring region now when
         self.locationController.stopMonitoringRegion()
 
         switch (UIApplication.sharedApplication().applicationState) {
@@ -83,24 +81,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AVAudioPlayerDelegate {
     func applicationDidEnterBackground(application: UIApplication) { }
     func applicationWillEnterForeground(application: UIApplication) { }
     func applicationDidBecomeActive(application: UIApplication) { }
-    func applicationWillTerminate(application: UIApplication) { }
+    func applicationWillTerminate(application: UIApplication) {
+
+    }
 
     // MARK: - Core Data stack
     lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.idrisr.CoreDataDeleteMe" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         return urls[urls.count-1]
     }()
 
     lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
         let modelURL = NSBundle.mainBundle().URLForResource("\(self.model)", withExtension: "momd")!
         return NSManagedObjectModel(contentsOfURL: modelURL)!
     }()
 
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
         let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("\(self.db).sqlite")
 
