cea3467891cb42500ce488dc0db5ef7ff6c5a459
Idris Raja <idris.raja@gmail.com>
2016-05-14 14:41:35 -0500

    add XCode warnings for fixme warnings

---
+++ warnings.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+TAGS="FIXME:"
+echo "searching ${SRCROOT} for ${TAGS}"
+
+find "${SRCROOT}" \( -name "*.swift" \) -print0 |\
+xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" |\
+perl -p -e "s/($TAGS)/ warning: \$1/"
